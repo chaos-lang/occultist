d4c66ab6ae559bc3a182db32fbc0207a8e3e0267
Noah Altunian <nbaltunian@gmail.com>
2020-12-03 16:20:39 -0500

    Remove unneeded sigils
    I forgot to remove this in 7a5dd6e

--- occultist.sh
+++ occultist.sh
@@ -102,11 +102,11 @@ mingw_is_admin() {
 
 get_latest_tag_or_default_branch() {
     local BRANCH
-    $BRANCH=$(git ls-remote --tags --refs ${1} | tail -n1 | sed 's/.*\///')
+    BRANCH=$(git ls-remote --tags --refs ${1} | tail -n1 | sed 's/.*\///')
     if [ -z $BRANCH ]; then
         git init &> /dev/null
-        $BRANCH=$(git remote show ${1} | grep "HEAD branch" | cut -d ":" -f 2)
-        $BRANCH="${BRANCH:1}"
+        BRANCH=$(git remote show ${1} | grep "HEAD branch" | cut -d ":" -f 2)
+        BRANCH="${BRANCH:1}"
     fi
     echo $BRANCH
 }
